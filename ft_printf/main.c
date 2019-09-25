5c5afd26f77e71d4e3b09a554e245cf72fd42cb4
FoxEL <foxel1993@gmail.com>
2019-09-25 14:47:37 +0300

    add

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <limits.h>
+#include <stdlib.h>
 
 //FLAG
 #define F_HASH  0x80
@@ -14,25 +15,53 @@
 #define F_APOS  0x04
 #define F_ARAB  0x02
 
-#define TYPE_STR    " diouxXeEfFgGaAcspn"
+#define TYPE_STR    " diouxXeEfFgGaAcspn%"
+
 typedef struct      s_str
 {
-    char *start;
-    char *end;
-
-    unsigned char   flag;    //#0- +'|
+    char            *str;
+    char            flag;    //#0- +'|
     int             width;
     int             accuracy;
-    char            mod_length; //hh,h,I,II,L,q,j,z,t
-    char            type;       //d,i,o,u,x,X,e,E,f,F,g,G,a,A,c,s,p,n
-    va_list         arg;
+    char            mod_length; //hh,h,l,ll,L,q,j,z,t
+    char            type;       //d,i,o,u,x,X,e,E,f,F,g,G,a,A,c,s,p,n,%
     struct s_str    *next;
     struct s_str    *prev;
 }                   t_str;
 
+int ft_putchar(char c)
+{
+    write(1, &c, 1);
+    return (0);
+}
+
+int ft_putnstr(char *str,size_t n)
+{
+    write(1, str, n);
+    return (0);
+}
+
+int ft_putstr(char *str)
+{
+    while (*str)
+        write(1, str++, 1);
+    return (0);
+}
+
+int ft_isdigit(int c)
+{
+    return (c >= '0' && c <= '9');
+}
+
+int ft_error(void)
+{
+    ft_putnstr("ERROR\n", 6);
+    return (0);
+}
+
 int     ft_flag_str(const char **str)
 {
-    unsigned char flag;
+    int flag;
 
     flag = 0;
     while (*(++(*str)))
@@ -64,16 +93,17 @@ int     ft_width_str(const char **str, va_list *arg)
     a = 0;
     if (**str == '*') {
         (*str)++;
-        a = va_arg(*arg, int);
-        return (a);
+        return (va_arg(*arg, int));
     }
-    while (isdigit(**str) && a < INT_MAX)
+    if (!ft_isdigit(**str))
+        return (-1);
+    while (ft_isdigit(**str) && a < INT_MAX)
     {
         a = a * 10 + (**str - '0');
         (*str)++;
     }
     if (a > INT_MAX)
-        return (-1);
+        return (0);
     return ((int)a);
 }
 
@@ -84,24 +114,24 @@ int     ft_accuracy_str(const char **str, va_list *arg)
         (*str)++;
         return (ft_width_str(str, arg));
     }
-    return (0);
+    return (-1);
 }
 
-int     ft_modificator_str(const char **str)
+int     ft_modification_str(const char **str)
 {
     if (**str == 'h' && *((*str) + 1) == 'h')
     {
         (*str) += 2;
         return (1);
     }
-    if (**str == 'I' && *((*str) + 1) == 'I')
+    if (**str == 'l' && *((*str) + 1) == 'l')
     {
         (*str) += 2;
         return (4);
     }
     if (**str == 'h' && (*str)++)
         return (2);
-    if (**str == 'I' && (*str)++)
+    if (**str == 'l' && (*str)++)
         return (3);
     if (**str == 'L' && (*str)++)
         return (5);
@@ -116,12 +146,12 @@ int     ft_modificator_str(const char **str)
     return (0);
 }
 
-int     ft_type_str(const char **str)
+int     ft_type_str(const char **str, va_list *arg)
 {
     int i;
 
     i = 1;
-    while (i < 18)
+    while (i < 20)
     {
         if (**str == TYPE_STR[i])
         {
@@ -130,25 +160,50 @@ int     ft_type_str(const char **str)
         }
         i++;
     }
-    return (0);
+    return (-1);
 }
 
-
-/*t_str   ft_newlst_str(char *str)
+t_str   *ft_newlst_str(char *str)
 {
+    t_str *list;
 
-}*/
+    if (!(list = (t_str *)malloc(sizeof(t_str))))
+        return (NULL);
+    list->flag = -1;
+    list->accuracy = -1;
+    list->width = -1;
+    list->type = -1;
+    list->mod_length = -1;
+    list->next = NULL;
+    list->prev = NULL;
 
-int ft_putchar(char c)
-{
-    write(1, &c, 1);
-    return (0);
+    return (list);
 }
 
-int ft_putnstr(char *str,size_t n)
+void    ft_addlst_str(t_str **current, t_str **list)
 {
-    write(1, str, n);
-    return (0);
+    if (*current == NULL)
+        *current = *list;
+    else {
+        (*list)->prev = *current;
+        (*current)->next = *list;
+    }
+}
+
+char    *ft_newstr_str(const char **format, va_list *args)
+{
+    int mas[5];
+
+    mas[0] = ft_flag_str(format);
+    mas[1] = ft_width_str(format, args);
+    mas[2] = ft_accuracy_str(format, args);
+    mas[3] = ft_modification_str(format);
+    mas[4] = ft_type_str(format, args);
+    //if (mas[4] == 0)
+    //    return (NULL);
+    if (!mas[0] && (mas[1] + mas[2]) == -2 && !mas[3] && mas[4] == 19)      //0 -1 -1 0 #
+       ft_putstr("%");
+    return (NULL);
 }
 
 int ft_printf(const char *format, ...)
@@ -157,44 +212,28 @@ int ft_printf(const char *format, ...)
     int		ret;
     char    *str;
     char    *tmp;
-    int flag;
-    int w;
-    int a;
-    int m;
-    int t;
 
+    t_str *head;
+
+    head = NULL;
     va_start(args, format);
-    str = (char *)format;
-    while (*format)
+    while (*format != '\0')
     {
+        str = (char *)format;
         while (*format != '\0' && *format != '%')
             format++;
-        if (*(format + 1) == '%')
-        {
-            format++;
-            continue ;
-        }
-        if ((flag = ft_flag_str(&format)) != -1)
-            printf ("%d ",flag);
-        if ((w = ft_width_str(&format, &args)) != -1)
-            printf ("%d ",w);
-        if ((a = ft_accuracy_str(&format, &args)) != -1)
-            printf ("%d ",a);
-        if ((m = ft_modificator_str(&format)) != -1)
-            printf ("%d ",m);
-        if ((t = ft_type_str(&format)) != -1)
-            printf ("%d ",t);
+
+
+        ft_newstr_str(&format, &args);
         printf ("\t %s",format);
+        str = (char *)format;
         printf ("\n");
-        //format++;
     }
-    /*if (*str)
-        ft_putnstr(str, format - str);*/
     va_end(args);
     return (0);
 }
 
 int main() {
-    ft_printf("%%*.*",2,1,3,4);
+    ft_printf("%# 123dd",2,1,3,4);
     return 0;
 }
